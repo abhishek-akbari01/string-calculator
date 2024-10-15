da4886727640db568f8a2e50f87b461cca97b0f9
Abhishek Akbari <abhishekakbari9668@gmail.com>
2024-10-15 15:26:12 +0530

    Test: Add test for single number input

--- src/utils/add.test.ts
+++ src/utils/add.test.ts
@@ -3,3 +3,8 @@ import { add } from "./add";
 test('returns 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('returns the number for a single number input', () => {
+    expect(add("1")).toBe(1);
+});
+  
