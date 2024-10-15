0d0988898c5ef6ba4813d2fee19683b1eee5f599
Abhishek Akbari <abhishekakbari9668@gmail.com>
2024-10-15 16:01:16 +0530

    Feat: Support custom delimiters

--- src/utils/add.ts
+++ src/utils/add.ts
@@ -1,7 +1,18 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0;
-    const formattedNumbers = numbers.replace(/\n/g, ',');
-    const numArray = formattedNumbers.split(',').map(Number);
+    let delimiter = ',';
+
+    if (numbers.startsWith('//')) {
+        delimiter = numbers[2];
+        numbers = numbers.slice(4);
+    }
+    console.log("numbers-", numbers);
+    // console.log("Delimeter-", delimiter);
+
+    const formattedNumbers = numbers.replace(/\n/g, delimiter);
+    // console.log("formattedNumbers-", formattedNumbers);
+
+    const numArray = formattedNumbers.split(delimiter).map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
 }
   
