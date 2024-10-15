12adc440df1528f2b37c8398ac696f08af39325b
Abhishek Akbari <abhishekakbari9668@gmail.com>
2024-10-15 15:46:50 +0530

    Feat: Add support for new lines between numbers

--- src/utils/add.ts
+++ src/utils/add.ts
@@ -1,6 +1,7 @@
 export function add(numbers: string): number {
     if (numbers === "") return 0;
-    const numArray = numbers.split(',').map(Number);
+    const formattedNumbers = numbers.replace(/\n/g, ',');
+    const numArray = formattedNumbers.split(',').map(Number);
     return numArray.reduce((sum, num) => sum + num, 0);
 }
   
